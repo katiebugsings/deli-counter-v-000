9886696fa7b2393437f42405d2d07294154f581c
Katie Cassell <kcassell944@gmail.com>
2018-02-16 15:16:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,6 @@ def now_serving(katz_deli)
   if katz_deli.count == 0
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.shift
-
+    puts "Currently serving #{ katz_deli }."
   end
 end
